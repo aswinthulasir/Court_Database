fdee67ee26073c27441a90b08b62a6ec316ddf0e
Aswin Thulasi R <125785982+Aswinrthulasi@users.noreply.github.com>
2024-07-22 13:28:17 +0530

    Initial_commit_2.0

---
+++ subdistrict.sql
@@ -0,0 +1,66 @@
+-- Create the table
+CREATE TABLE KeralaCourtComplexes (
+    District VARCHAR(50),
+    SubDistrict VARCHAR(50),
+    CourtComplexName VARCHAR(100)
+);
+
+-- Insert data into the table
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Alappuzha', 'Alappuzha', 'Alappuzha District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Alappuzha', 'Cherthala', 'Cherthala Sub-District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Alappuzha', 'Ambalappuzha', 'Ambalappuzha Sub-District Court');
+
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Ernakulam', 'Ernakulam', 'Ernakulam District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Ernakulam', 'Aluva', 'Aluva Sub-District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Ernakulam', 'Perumbavoor', 'Perumbavoor Sub-District Court');
+
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Idukki', 'Idukki', 'Idukki District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Idukki', 'Thodupuzha', 'Thodupuzha Sub-District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Idukki', 'Peermade', 'Peermade Sub-District Court');
+
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Kannur', 'Kannur', 'Kannur District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Kannur', 'Thalassery', 'Thalassery Sub-District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Kannur', 'Payyannur', 'Payyannur Sub-District Court');
+
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Kasaragod', 'Kasaragod', 'Kasaragod District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Kasaragod', 'Hosdurg', 'Hosdurg Sub-District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Kasaragod', 'Manjeshwar', 'Manjeshwar Sub-District Court');
+
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Kollam', 'Kollam', 'Kollam District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Kollam', 'Kottarakkara', 'Kottarakkara Sub-District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Kollam', 'Punalur', 'Punalur Sub-District Court');
+
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Kottayam', 'Kottayam', 'Kottayam District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Kottayam', 'Pala', 'Pala Sub-District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Kottayam', 'Changanassery', 'Changanassery Sub-District Court');
+
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Kozhikode', 'Kozhikode', 'Kozhikode District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Kozhikode', 'Vadakara', 'Vadakara Sub-District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Kozhikode', 'Koyilandy', 'Koyilandy Sub-District Court');
+
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Malappuram', 'Malappuram', 'Malappuram District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Malappuram', 'Manjeri', 'Manjeri Sub-District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Malappuram', 'Ponnani', 'Ponnani Sub-District Court');
+
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Palakkad', 'Palakkad', 'Palakkad District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Palakkad', 'Ottappalam', 'Ottappalam Sub-District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Palakkad', 'Mannarkkad', 'Mannarkkad Sub-District Court');
+
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Pathanamthitta', 'Pathanamthitta', 'Pathanamthitta District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Pathanamthitta', 'Thiruvalla', 'Thiruvalla Sub-District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Pathanamthitta', 'Adoor', 'Adoor Sub-District Court');
+
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Thiruvananthapuram', 'Thiruvananthapuram', 'Thiruvananthapuram District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Thiruvananthapuram', 'Neyyattinkara', 'Neyyattinkara Sub-District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Thiruvananthapuram', 'Attingal', 'Attingal Sub-District Court');
+
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Thrissur', 'Thrissur', 'Thrissur District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Thrissur', 'Irinjalakuda', 'Irinjalakuda Sub-District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Thrissur', 'Chalakudy', 'Chalakudy Sub-District Court');
+
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Wayanad', 'Kalpetta', 'Kalpetta District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Wayanad', 'Mananthavady', 'Mananthavady Sub-District Court');
+INSERT INTO KeralaCourtComplexes (District, SubDistrict, CourtComplexName) VALUES ('Wayanad', 'Sulthan Bathery', 'Sulthan Bathery Sub-District Court');
+
+-- Example query to retrieve all court complexes
+SELECT * FROM KeralaCourtComplexes;
